dee3b042c3536c5a30e35c8aa2bf3475a12c365e
Scott Yang <scott.yang@5thkind.com>
2017-09-03 21:36:03 -0700

    Allow dynamic disabling of tooltip.

--- src/calendar-heatmap.js
+++ src/calendar-heatmap.js
@@ -187,17 +187,23 @@ function calendarHeatmap() {
 
       if (chart.tooltipEnabled()) {
         dayRects.on('mouseover', function (d, i) {
-          tooltip = d3.select(chart.selector())
-            .append('div')
-            .attr('class', 'day-cell-tooltip')
-            .html(tooltipHTMLForDate(d))
-            .style('left', function () { return Math.floor(i / 7) * SQUARE_LENGTH + 'px'; })
-            .style('top', function () {
-              return formatWeekday(d.getDay()) * (SQUARE_LENGTH + SQUARE_PADDING) + MONTH_LABEL_PADDING * 2 + 'px';
-            });
+          if (chart.tooltipEnabled()) {
+            tooltip = d3.select(chart.selector())
+              .append('div')
+              .attr('class', 'day-cell-tooltip')
+              .html(tooltipHTMLForDate(d))
+              .style('left', function () {
+                return Math.floor(i / 7) * SQUARE_LENGTH + 'px';
+              })
+              .style('top', function () {
+                return formatWeekday(d.getDay()) * (SQUARE_LENGTH + SQUARE_PADDING) + MONTH_LABEL_PADDING * 2 + 'px';
+              });
+          }
         })
         .on('mouseout', function (d, i) {
-          tooltip.remove();
+          if (tooltip) {
+            tooltip.remove();
+          }
         });
       }
 
